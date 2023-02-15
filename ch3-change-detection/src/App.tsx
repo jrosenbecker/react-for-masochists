16eb34673c6835fd80de12d0308bca427dac0d02
jrosenbecker-redox <jrosenbecker@redoxengine.com>
2023-02-15 10:24:27 -0600

    Add ch3 module

---
+++ ch3-change-detection/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Counter } from './Counter';
+
+function App() {
+  return (
+    <div className="App flex align-center justify-center p4">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
---
+++ ch3-change-detection/src/Counter/Counter.tsx
@@ -0,0 +1,23 @@
+import { FC, useState } from 'react';
+
+
+
+export const Counter: FC = () => {
+  const [count, setCount] = useState(0);
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  }
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="flex flex-row">
+      <button onClick={decrementCount}>-</button>
+      <div className="m4">{count}</div>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  )
+}
